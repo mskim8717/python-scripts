344297f2baf5a2682b4181b5c5a780c52b64d166
minsooKim <ms.kim8717@gmail.com>
2021-11-25 14:56:50 +0900

    [웹소켓] 샘플 코드 업로드

---
+++ 웹소켓/websocket_sample1.py
@@ -0,0 +1,15 @@
+import asyncio
+
+
+async def add(a, b):
+    print('add: {0} + {1}'.format(a, b))
+    await asyncio.sleep(1.0)  # 1초 대기. asyncio.sleep도 네이티브 코루틴
+    return a + b  # 두 수를 더한 결과 반환
+
+
+async def main(a, b):
+    result = await add(a, b)  # await로 다른 네이티브 코루틴 실행하고 반환값을 변수에 저장
+    print('print_add: {0} + {1} = {2}'.format(a, b, result))
+
+
+asyncio.run(main(1,2))
